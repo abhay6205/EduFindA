ece6fe737f1758c770bdc482aba4356627f5f912
ABHAY KUMAR <abhay11082005@gmail.com>
2026-05-27 23:37:09 +0530

    adding file

---
+++ backend/app/api/__init__.py
@@ -0,0 +1,5 @@
+from .auth import router as auth_router
+from .schools import router as schools_router
+from .teachers import router as teachers_router
+from .ads import router as ads_router
+from .reviews import router as reviews_router
