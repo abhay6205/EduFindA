ce7a539edeeb9d59d1f745d1979ebdca782a5d7b
ABHAY KUMAR <abhay11082005@gmail.com>
2026-05-27 23:46:28 +0530

    Add backend/app/schemas/__init__.py

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,8 @@
+from .all_schemas import (
+    UserBase, UserCreate, UserResponse, LoginData,
+    SchoolBase, SchoolCreate, SchoolResponse, SchoolDetailResponse,
+    TeacherBase, TeacherCreate, TeacherResponse,
+    ImageBase, ImageCreate, ImageResponse,
+    AdBase, AdCreate, AdResponse,
+    ReviewBase, ReviewCreate, ReviewResponse
+)
